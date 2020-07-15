f59b1201d6dd8afe6504c63cb58314fb30dc5979
ChristineAjapuhnya <68324305+ChristineAjapuhnya@users.noreply.github.com>
2020-07-16 08:27:26 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+for (let index = 0; index < 2; index++) {
+    basic.showString("Bonjour")
+    basic.pause(500)
+    basic.showNumber(10853)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+}
+basic.forever(function () {
+    basic.showString("Bonjour")
+    basic.pause(500)
+    basic.showNumber(10853)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+})
